642a4263eefdc61e30f1f4bc4b07b772680633fa
Bhuvan <bhuvanshyam2022@gmail.com>
2024-08-04 13:27:39 +0530

    added serverconf.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { PORT } = require("../serverConfig");
+const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = () => {
   const app = express();
